6a2546f993778b8742434144273f0cc782c0474f
sampath20xt <153092304+sampath20xt@users.noreply.github.com>
2024-10-09 15:14:28 +0530

    Update Model_now.py

--- Model_now.py
+++ Model_now.py
@@ -5,7 +5,6 @@ import textwrap
 import os
 import json
 
-
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "email-extraction-381718-3f73208ce3b71.json"
 vertexai.init(project="email-extraction-381718", location="us-central1")
 generative_multimodal_model = GenerativeModel("gemini-1.5-pro-001")
@@ -15,6 +14,7 @@ generative_multimodal_model = GenerativeModel("gemini-1.5-pro-001")
 def chunk_text(text, token_limit=8192):
     chunks = textwrap.wrap(text, width=token_limit)
     return chunks
+
 
 # Function to evaluate the spoken text using Vertex AI and Gemini model
 def evaluate_text(choose_topic, spoken_text):
@@ -49,7 +49,8 @@ def evaluate_text(choose_topic, spoken_text):
         HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_ONLY_HIGH,
     }
 
-    response = generative_multimodal_model.generate_content(prompt, generation_config=generation_config, safety_settings=safety_settings)
+    response = generative_multimodal_model.generate_content(prompt, generation_config=generation_config,
+                                                            safety_settings=safety_settings)
     return response.text
 
 
@@ -81,7 +82,8 @@ def paragraphs(choose_topic):
         HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_ONLY_HIGH,
         HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_ONLY_HIGH,
     }
-    response = generative_multimodal_model.generate_content(prompt, generation_config=generation_config, safety_settings=safety_settings)
+    response = generative_multimodal_model.generate_content(prompt, generation_config=generation_config,
+                                                            safety_settings=safety_settings)
     return response.text
 
 
@@ -119,7 +121,7 @@ def generate_skills_and_topics(title, job_designation, experience_range):
     response = generative_multimodal_model.generate_content(prompt, generation_config=generation_config,
                                                             safety_settings=safety_settings)
     # Print the raw response for debugging
-    print(f"Model response: {response.text}")
+    # print(f"Model response: {response.text}")
     try:
         cleaned_response = response.text.replace("```python", "").replace("```", "").strip()
         cleaned_response = cleaned_response.replace("'", '"')
@@ -174,10 +176,11 @@ def generate_questions(topics, question_type, difficulty_level, num_questions, a
 
             For 'pseudo code' type questions:
             - Generate pseudo type of questions mainly focusing on coding and programming related to the topics. 
-            - Based on the difficulty level generate the pseudo questions.
+            - Based on the difficulty level generate the pseudo questions only do not give any examples.
 
             For 'subjective' type questions:
             - Generate Subjective type of questions, A clear and open-ended question related to the topic.
+            - Based on the difficulty level generate the subjective questions only do not give any examples.
 
             If the difficulty_level is 'easy', don't be too basic while generating the questions. 
             Ensure that the questions are diverse, testing both theoretical knowledge and practical problem-solving skills.
@@ -186,6 +189,8 @@ def generate_questions(topics, question_type, difficulty_level, num_questions, a
             Include a mixture of basic and advanced scenarios, ensuring the difficulty is scaled as per the specified level.
             If the user gives any additional requirements, implement that while generating the questions.
             For 'MCQ' type of questions,The output should be a list of dictionaries which consist of question, options, and correct answer. 
+            For 'pseudo code' type of questions,The output should be a list of dictionaries which consist of question, and correct answer. 
+            For 'subjective' type of questions,The output should be a list of dictionaries which consist of question, and correct answer. 
             For 'project' type of questions,The output should be a list of dictionaries which consist of Scenario, Task and Test Cases.
             Make sure that the questions are relevant to the given topics and are programming-related.
             skills: {topics}
@@ -233,7 +238,7 @@ def evaluate_answers(questions, answers):
         user_answers: {answers}
         correct_answer:
         evaluation: 
-        The output should be a list of dictionaries with questions, user_answers, correct_answer, and evaluation. Don't wrap any text to the output.
+        The output should be a list of dictionaries with questions, user_answers, correct_answer, and evaluation. Don't wrap any text or strings to the output.
         """
     generation_config = {
         "max_output_tokens": 8192,
@@ -260,17 +265,19 @@ def evaluate_answers(questions, answers):
                                                                 safety_settings=safety_settings)
         return response.text.strip()
 
-def validate_testCases(questions, answers, test_cases):
+
+def validate_testCases(sceranio, task, code, test_cases):
     prompt = f"""
     you are expert Evaluation Specialist who are evaluating the effectiveness of assessments and exams.
     And you are trained on Quality Assurance (QA) Engineer who are responsible for testing the various test cases and scenarios.
-    your task is to evaluate the every given answer with question and test the given answer with given test cases.
-    You have to test and evaluate the given answer with given various test cases and scenarios.
-    If the answer passed in given test cases then the answer will be 'Passed'.
-    If the answer failed in given any test cases then the answer will be 'Failed'.
+    your task is to evaluate the every given code with scenario, task and test with given test cases.
+    You have to test and evaluate the given code with given various test cases and scenarios.
+    If the code passed in given test cases then the answer will be 'Passed'.
+    If the code failed in given any test cases then the answer will be 'Failed'.
     The output should be a list of dictionaries for rach question, with the following fields:
-        questions: {questions}
-        user_answers: {answers}
+        scenario: {sceranio}
+        task: {task}
+        user_answers: {code}
         test_cases:{test_cases}
         answer: 
     The output should be a list of dictionaries with questions, user_answers, test_cases, and answer. Don't wrap any text to the output.
